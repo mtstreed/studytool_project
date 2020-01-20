aa0655607e0ff2129a977f93632719439d40404e
mtstreed <42426712+mtstreed@users.noreply.github.com>
2020-01-20 11:09:59 +0100

    Update urls.py

--- studytool_project/urls.py
+++ studytool_project/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("studytool_app/", include("studytool_app.urls")),
-    path("", include("studytool_app.urls"))
 ]
